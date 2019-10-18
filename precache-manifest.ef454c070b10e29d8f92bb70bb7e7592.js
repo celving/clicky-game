7aa6e544355701560b69a8a553c474931737ebfd
Colin <viewtifuljho@hotmail.com>
2019-10-17 21:12:43 -0500

    Updates

---
+++ precache-manifest.ef454c070b10e29d8f92bb70bb7e7592.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f3dca0ea474b90d95c3434fa4528b9f0",
+    "url": "/clicky-game/index.html"
+  },
+  {
+    "revision": "2a1d2f49bfbe854014de",
+    "url": "/clicky-game/static/css/main.3bc15861.chunk.css"
+  },
+  {
+    "revision": "9501f22ec6282b72990a",
+    "url": "/clicky-game/static/js/2.99673906.chunk.js"
+  },
+  {
+    "revision": "2a1d2f49bfbe854014de",
+    "url": "/clicky-game/static/js/main.e2d1f2a2.chunk.js"
+  },
+  {
+    "revision": "9e63f44ec9a3dfc4c648",
+    "url": "/clicky-game/static/js/runtime-main.742e30a5.js"
+  }
+]);
